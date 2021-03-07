8ad72a79b119666607948f280afcb145d37e2b2b
DireMaster <pday32@gmail.com>
2021-03-07 21:35:05 +0000

    fix error.js

--- src/events/error.js
+++ src/events/error.js
@@ -2,7 +2,7 @@ module.exports = (client, error) => {
   client.logger.error(error);
   const errorLog = client.channels.cache.get(client.errorLogId);
   if (errorLog)
-    serverLog.send(
+    errorLog.send(
 			new MessageEmbed().setDescription(`Oh Dear MENTL just crashed heres why:
 																					\`${error}\``))
 };
