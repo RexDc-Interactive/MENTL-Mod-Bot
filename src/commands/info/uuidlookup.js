9e7146e542246628c41f3b37792c52562a3c2234
DireMaster <>
2021-04-15 20:14:37 +0000

    add uuid command

---
+++ src/commands/info/uuidlookup.js
@@ -0,0 +1,18 @@
+import { lookupName } from "namemc";
+
+module.exports = class UserInfoCommand extends Command {
+  constructor(client) {
+    super(client, {
+      name: 'mcuuid',
+      aliases: ['uuid'],
+      usage: 'uuid [player uuid]',
+      description: 'Fetches from NameMC the playername of the UUID provided.',
+      type: client.types.INFO,
+      examples: ['userinfo @Nettles']
+    });
+  }
+  async run(message, args) {
+		const user = await lookupUUID(args[1]);
+		message.channel.send(user);
+	}
+}
