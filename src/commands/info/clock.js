f1f996c0ad67b56c3a69a4cf720c5e48b62553e5
DireMaster <>
2021-05-03 13:52:53 +0000

    fix

--- src/commands/info/clock.js
+++ src/commands/info/clock.js
@@ -46,8 +46,8 @@ module.exports = class AdminsCommand extends Command {
 												"UTC+6 - Omsk, Asia" + moment().tz("Asia/Omsk").format("dddd h:mm a z") + "\n" +
 												"UTC+7 - Tomsk, Asia" + moment().tz("Asia/Tomsk").format("dddd h:mm a z") + "\n" +
                         "UTC+8 - Shanghai, China - " + moment().tz("Asia/Shanghai").format("dddd h:mm a z") + "\n" +
-												"UTC+8 - Singapore, Asia" + moment.tz("Asia/Singapore").format("dddd h:mm a z") + "\n" +
-												"UTC+9 - Tokyo, Japan - " + moment.tz("	Asia/Tokyo").format("dddd h:mm a z") + "\n" + "\n"
+												"UTC+8 - Singapore, Asia" + moment().tz("Asia/Singapore").format("dddd h:mm a z") + "\n" +
+												"UTC+9 - Tokyo, Japan - " + moment().tz("	Asia/Tokyo").format("dddd h:mm a z") + "\n" + "\n" +
                         "**Australia**" + "\n" +
                         "UTC+8 - Perth, Western Australia - " + moment().tz("Australia/Perth").format("dddd h:mm a z") + "\n" +
 												"UTC+10 - Brisbane, North Eastern Australia - " + moment().tz("Australia/Brisbane").format("dddd h:mm a z") + "\n" +
