1af3d70c03d12fc9e1cc7edbf07de056164705e1
DireMaster <>
2021-05-03 13:48:11 +0000

    Improved timezone command

--- src/commands/info/clock.js
+++ src/commands/info/clock.js
@@ -18,34 +18,43 @@ module.exports = class AdminsCommand extends Command {
         .setTitle('Time around the World')
         .setDescription(
 					              "**Western Pacific**" + "\n" +
-                        "Midway Islands - " + moment().tz("Pacific/Midway").format("dddd h:mm a z") + "\n" +
-												"Honolulu, Hawaii - " + moment().tz("Pacific/Honolulu").format("dddd h:mm a z") + "\n" +					
+                        "UTC-11 - Midway Islands - " + moment().tz("Pacific/Midway").format("dddd h:mm a z") + "\n" +
+												"UTC-10 - Honolulu, Hawaii - " + moment().tz("Pacific/Honolulu").format("dddd h:mm a z") + "\n" + "\n" +				
 					              "**America**" + "\n" +
-                        "Aleutian Islands	 - " + moment().tz("America/Adak").format("dddd h:mm a z") + "\n" +
-												"Los Angeles, USA - " + moment().tz("America/Los_Angeles").format("dddd h:mm a z") + "\n" +
-                        "Denver, USA - " + moment().tz("America/Denver").format("dddd h:mm a z") + "\n" + 
-                        "Chicago, USA - " + moment().tz("America/Chicago").format("dddd h:mm a z") + "\n" +
-                        "New York, USA - " + moment().tz("America/New_York").format("dddd h:mm a z") + "\n" +
-                        "Puerto Rico, USA - " + moment().tz("America/Puerto_Rico").format("dddd h:mm a z") + "\n" +
-                        "Sao Paulo, Brazil - " + moment().tz("America/Sao_Paulo").format("dddd h:mm a z") + "\n" + "\n" +
+                        "UTC-10 - Aleutian Islands - " + moment().tz("America/Adak").format("dddd h:mm a z") + "\n" +
+												"UTC-9 - Anchorage - " + moment().tz("America/Anchorage").format("dddd h:mm a z") + "\n" +
+												"UTC-8 - Los Angeles, USA - " + moment().tz("America/Los_Angeles").format("dddd h:mm a z") + "\n" +
+                        "UTC-7 - Denver, USA - " + moment().tz("America/Denver").format("dddd h:mm a z") + "\n" + 
+                        "UTC-6 - Chicago, USA - " + moment().tz("America/Chicago").format("dddd h:mm a z") + "\n" +
+                        "UTC-5 - New York, USA - " + moment().tz("America/New_York").format("dddd h:mm a z") + "\n" +
+                        "UTC-4 - Puerto Rico, USA - " + moment().tz("America/Puerto_Rico").format("dddd h:mm a z") + "\n" +
+                        "UTC-3 - Sao Paulo, Brazil - " + moment().tz("America/Sao_Paulo").format("dddd h:mm a z") + "\n" + "\n" +
+												"**Atlantic**" + "\n" +
+												"UTC-2 - South Georgia - " + moment().tz("Atlantic/South_Georgia").format("dddd h:mm a z") + "\n" +
+												"UTC-1 - Azores - " + moment().tz("Atlantic/Azores").format("dddd h:mm a z") + "\n" + "\n" + 
                         "**Europe**" + "\n" + 
 												"UTC - " + moment().tz("Etc/GMT").format("dddd h:mm a z") + "\n" +
-                        "London, UK - " + moment().tz("Europe/London").format("dddd h:mm a z") + "\n" +
-                        "Lisbon, Portugal - " + moment().tz("Europe/Lisbon").format("dddd h:mm a z") + "\n" +
-                        "Paris, France - " + moment().tz("Europe/Paris").format("dddd h:mm a z") + "\n" +
-                        "Athens, Greece - " + moment().tz("Europe/Athens").format("dddd h:mm a z") + "\n" + 
-                        "Moscow, Russia - " + moment().tz("Europe/Moscow").format("dddd h:mm a z") + "\n" + "\n" +
+                        "UTC - London, UK - " + moment().tz("Europe/London").format("dddd h:mm a z") + "\n" +
+                        "UTC - Lisbon, Portugal - " + moment().tz("Europe/Lisbon").format("dddd h:mm a z") + "\n" +
+                        "UTC+1 - Paris, France - " + moment().tz("Europe/Paris").format("dddd h:mm a z") + "\n" +
+                        "UTC+2 - Athens, Greece - " + moment().tz("Europe/Athens").format("dddd h:mm a z") + "\n" + 
+                        "UTC+4 - Moscow, Russia - " + moment().tz("Europe/Moscow").format("dddd h:mm a z") + "\n" + "\n" +
                         "**Africa**" + "\n" +
-                        "Johannesburg, South Africa - " + moment().tz("Africa/Johannesburg").format("dddd h:mm a z") + "\n" + "\n" +
+                        "UTC+2 - Johannesburg, South Africa - " + moment().tz("Africa/Johannesburg").format("dddd h:mm a z") + "\n" + "\n" +
                         "**Asia**" + "\n" +
-                        "Shanghai, China - " + moment().tz("Asia/Shanghai").format("dddd h:mm a z") + "\n" + "\n" +
+												"UTC+5 - Maldives, India" + moment().tz("Indian/Maldives").format("dddd h:mm a z") + "\n" +
+												"UTC+6 - Omsk, Asia" + moment().tz("Asia/Omsk").format("dddd h:mm a z") + "\n" +
+												"UTC+7 - Tomsk, Asia" + moment().tz("Asia/Tomsk").format("dddd h:mm a z") + "\n" +
+                        "UTC+8 - Shanghai, China - " + moment().tz("Asia/Shanghai").format("dddd h:mm a z") + "\n" +
+												"UTC+8 - Singapore, Asia" + moment.tz("Asia/Singapore").format("dddd h:mm a z") + "\n" +
+												"UTC+9 - Tokyo, Japan - " + moment.tz("	Asia/Tokyo").format("dddd h:mm a z") + "\n" + "\n"
                         "**Australia**" + "\n" +
-                        "Perth, Western Australia - " + moment().tz("Australia/Perth").format("dddd h:mm a z") + "\n" + 
-                        "Brisbane, North Eastern Australia - " + moment().tz("Australia/Brisbane").format("dddd h:mm a z") + "\n" +
-                        "Adelaide, Eastern Australia - " + moment().tz("Australia/Adelaide").format("dddd h:mm a z") + "\n" +
-                        "Melbourne, South Eastern Australia - " + moment().tz("Australia/Melbourne").format("dddd h:mm a z") + "\n" + "\n" +
+                        "UTC+8 - Perth, Western Australia - " + moment().tz("Australia/Perth").format("dddd h:mm a z") + "\n" +
+												"UTC+10 - Brisbane, North Eastern Australia - " + moment().tz("Australia/Brisbane").format("dddd h:mm a z") + "\n" +
+                        "UTC+10 - Melbourne, South Eastern Australia - " + moment().tz("Australia/Melbourne").format("dddd h:mm a z") + "\n" + "\n" +
                         "**Eastern Pacific**" + "\n" +
-                        "Auckland, New Zealand - " + moment().tz("Pacific/Auckland").format("dddd h:mm a z")
+												"UTC+11 - Norfolk - " + moment().tz("Pacific/Norfolk").format("dddd h:mm a z") + "\n" +
+                        "UTC+12 - Auckland, New Zealand - " + moment().tz("Pacific/Auckland").format("dddd h:mm a z")
         )
 		
         .setFooter(message.member.displayName, message.author.displayAvatarURL({ dynamic: true }))
