526ef6aec81b5b5f816bb45b1ca05c3aa264fa60
DireMaster <>
2021-05-17 10:25:51 +0000

    added Minecraft Name History command

--- src/commands/info/namehistory.js
+++ src/commands/info/namehistory.js
@@ -6,8 +6,8 @@ const { lookupName } = require('namemc');
 module.exports = class UserInfoCommand extends Command {
   constructor(client) {
     super(client, {
-      name: 'mcname',
-      aliases: ['playername'],
+      name: 'namehistory',
+      aliases: ['playernamehistory'],
       usage: 'mcname [playername]',
       description: 'Fetches from NameMC names matching the specified name.',
       type: client.types.INFO,
@@ -18,9 +18,15 @@ module.exports = class UserInfoCommand extends Command {
 		const users = await lookupName(args[0]);
 		message.channel.send("These Players have or did have this name: ")
 		users.map(user => {
-			message.channel.send("This players current name is: " + user.currentName)
-			message.channel.send("There UUID is: " + user.uuid)
-			message.channel.send("")
-		});
+			message.channel.send(" ")
+			message.channel.send(user.currentName)
+			message.channel.send("===========")
+			user.pastNames.map(pastNames => {
+				const milliseconds = pastNames.changedAt // 1575909015000
+				const dateObject = new Date(milliseconds)
+				const changedat = dateObject.toLocaleString() //2019-12-9 10:30:15
+				message.channel.send(pastNames.name + " - " + changedat)
+		})
+	})
 	}
 }
